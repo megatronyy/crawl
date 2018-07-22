0d3c5c9af24bdb1ef7cd6b022de3b9e0c1bb00c3
twfx7 <twfx7758@163.com>
2018-07-22 18:07:47 +0800

    gitignore

--- rule/city58/city58.go
+++ rule/city58/city58.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/henrylee2cn/pholcus/app/spider"
 	"github.com/henrylee2cn/pholcus/app/downloader/request"
 	"strconv"
+	"github.com/henrylee2cn/pholcus/common/goquery"
 )
 
 func init() {
