d6b951ece07dfac64e60efa89798151dfc58d198
屈文斌 <twfx7758@hotmail.com>
2018-08-25 14:10:18 +0800

    优化代码

--- start.sh
+++ start.sh
@@ -4,10 +4,26 @@ set -e
 
 cd /usr/bin
 
+# 判断是否有执行权限
 if [ ! -x crawl ];then
+    # 添加执行权限
     chmod +x crawl
     
-    cd /home/crawl
-    
-    /usr/bin/crawl -_ui=cmd -a_mode=0 -c_spider=0 -a_outtype=mysql -a_thread=10 -a_dockercap=30 -a_pause=5000 -a_proxyminute=0 -a_keyins="<crawl><golang>" -a_success=true -a_failure=true
 fi
+
+# 跳转目录
+cd /home/crawl
+# 后台启动爬虫
+nohup /usr/bin/crawl \
+-_ui=cmd \
+-a_mode=0 \
+-c_spider=0 \
+-a_outtype=mysql \
+-a_thread=10 \
+-a_dockercap=30 \
+-a_pause=5000 \
+-a_proxyminute=0 \
+-a_keyins="<crawl><golang>" \
+-a_success=true -a_failure=true &
+
+echo "crawl start..."
