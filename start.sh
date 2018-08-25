fda837019b7f02790839a43165b5a2c8c2a1e4b9
屈文斌 <twfx7758@hotmail.com>
2018-08-25 14:00:51 +0800

    优化代码

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ cd /usr/bin
 
 if [ ! -x crawl ];then
     chmod +x crawl
-
-cd /home/crawl
-
-/usr/bin/crawl -_ui=cmd -a_mode=0 -c_spider=0 -a_outtype=mysql -a_thread=10 -a_dockercap=30 -a_pause=5000 -a_proxyminute=0 -a_keyins="<crawl><golang>" -a_success=true -a_failure=true
+    
+    cd /home/crawl
+    
+    /usr/bin/crawl -_ui=cmd -a_mode=0 -c_spider=0 -a_outtype=mysql -a_thread=10 -a_dockercap=30 -a_pause=5000 -a_proxyminute=0 -a_keyins="<crawl><golang>" -a_success=true -a_failure=true
+fi
