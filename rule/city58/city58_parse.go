36ac66ea669dac4613faf829d56b89742d3493d8
屈文斌 <twfx7758@hotmail.com>
2018-08-26 12:32:13 +0800

    独立解析函数

--- rule/city58/city58_parse.go
+++ rule/city58/city58_parse.go
@@ -7,17 +7,18 @@ import (
 func city58_cz_parse(ctx *Context) {
 	query := ctx.GetDom()
 
-	var 标题, 价格, 单位, 出租价格, 出租单位, 面积, 类型, 经营状态, 历史经营, 付款方式, 租约方式, 详细地址, 发布时间, 发布人, 联系电话, 详情 string
+	var 标题, 价格, 单位, 出租价格, 出租单位, 面积, 类型, 经营状态, 历史经营, 付款方式, 租约方式, 详细地址, 发布时间, 发布人, 联系电话, 电话归属, 详情 string
 
-	标题 = query.Find("div.house-title h1").Text()
-	价格 = query.Find(".house_basic_title_money_num").Text()
-	单位 = query.Find(".house_basic_title_money_unit").Text()
-	出租价格 = query.Find(".house_basic_title_money_num_chuzu").Text()
-	出租单位 = query.Find(".house_basic_title_money_unit_chuzu").Text()
-	面积 = query.Find(".house-basic-right .house_basic_title_content").Eq(1).Find(".house_basic_title_content_item2").Text()
-
-
-
+	标题 = gettitle(query)
+	价格 = getprice(query)
+	单位 = getuint(query)
+	出租价格 = getczprice(query)
+	出租单位 = getczuint(query)
+	面积, 类型, 经营状态, 历史经营, 付款方式, 租约方式, 详细地址 = getother(query)
+	发布时间 = publishtime(query)
+	发布人 = publisher(query)
+	联系电话, 电话归属 = publisherphone(query)
+	详情 = getdetails(query)
 
 	ctx.Output(map[int]interface{}{
 		0:  标题,
@@ -35,6 +36,7 @@ func city58_cz_parse(ctx *Context) {
 		12: 发布时间,
 		13: 发布人,
 		14: 联系电话,
-		15: 详情,
+		15: 电话归属,
+		16: 详情,
 	})
 }
